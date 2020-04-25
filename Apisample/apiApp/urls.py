a8242078ff26220149d097b244e3283079835728
gwendal <gwendal@debian>
2020-04-25 20:09:07 +0200

    Pirncipe et valeurs ajoutées

--- Apisample/apiApp/urls.py
+++ Apisample/apiApp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .apiviews import ValueList, ValueDetail
+from .apiviews import PrincipleList, PrincipleDetail
 
 urlpatterns = [
-    path('values', )
+    path('values/', ValueList.as_view(), name='values_list'),
+    path('values/<int:pk>', ValueDetail.as_view(),name='value_detail'),
+    path('principles/', PrincipleList.as_view(), name='principles_list'),
+    path('principles/<int:pk>/', PrincipleDetail.as_view(), name='principle_detail'),
 ]
