217844bbb6ceafd111c242b0d4f5ef74e0b30e67
gwendalF <gwendalfernet@gmail.com>
2020-04-30 16:52:04 +0200

    renommages fichiers

--- api_sample/api_sample/asgi.py
+++ api_sample/api_sample/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ApiSample project.
+ASGI config for api_sample project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ApiSample.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_sample.settings')
 
 application = get_asgi_application()
--- api_sample/api_sample/urls.py
+++ api_sample/api_sample/urls.py
@@ -1,4 +1,4 @@
-"""ApiSample URL Configuration
+"""api_sample URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ApiApp.urls')),
+    path('', include('api_app.urls')),
 ]
